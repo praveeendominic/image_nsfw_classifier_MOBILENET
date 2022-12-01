a8c398f07161dd428ac03335e156a7143767e3fa
praveeendominic <61731613+praveeendominic@users.noreply.github.com>
2022-12-01 18:00:14 -0500

    image nudity detector app

--- st_image_tester.py
+++ st_image_tester.py
@@ -10,7 +10,7 @@ model_path = Path('model_mobilenet')/ 'saved_model.h5'
 model=predict.load_model(model_path)
 
 
-st.header('TEST IMAGE NUDITY')
+st.header('IMAGE NUDITY DETECTOR')
 st.write("Please select an image for nudity check.")
 
 image_uploaded=st.file_uploader(label= "Please upload a file",accept_multiple_files=False, type = ['jpg', 'jpeg', 'png'])
@@ -20,6 +20,11 @@ if image_uploaded is not None:
 
     # uploaded_image_path=os.path.join('tmp_dir',image_uploaded.name)
     # st.write(uploaded_image_path)
+    fname=os.path.splitext(image_uploaded.name)
+    file_ext=fname[1]
+    # st.write(file_ext)
+    fname_u='tmp'+file_ext
+    st.write(fname_u)
     
     # saving file
     with open(image_uploaded.name, 'wb') as f:
